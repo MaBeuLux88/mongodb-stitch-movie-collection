1d7835d4deafffed0431e30b6c4413c2ee925628
Maxime Beugnet <maxime.beugnet@gmail.com>
2018-09-20 03:04:36 +0200

    improve the whitespace handeling in the movie title

--- IMDB/post.sh
+++ IMDB/post.sh
@@ -4,5 +4,4 @@ then
   echo "Missing title argument!";
   exit 1
 fi
-body='{"Title":"'${1}'"}'
-curl -H "Content-Type: application/json" -d "$body" https://webhooks.mongodb-stitch.com/api/client/v2.0/app/stitchtest-adsgi/service/IMDB/incoming_webhook/post_movie_title?secret=test
+curl -H "Content-Type: application/json" -d "{\"Title\":\"${1}\"}" https://webhooks.mongodb-stitch.com/api/client/v2.0/app/stitchtest-adsgi/service/IMDB/incoming_webhook/post_movie_title?secret=test
